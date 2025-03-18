81352deb090bb60fee17ebce554867df60822847
sohial1265 <afarmeyo8@gmail.com>
2025-03-18 22:37:55 +0500

    Set routes

---
+++ routes/urlRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const urlController = require('./controllers/urlController');
+
+router.post('/shorten', urlController.createShortUrl);
+router.get('/shorten/:shortCode', urlController.getShortUrl);
+router.put('/shorten/:shortCode', urlController.updateShortUrl);
+router.delete('/shorten/:shortCode', urlController.deleteShortUrl);
+router.get('/shorten/:shortCode/stats', urlController.getUrlStats);
+
+module.exports = router;
