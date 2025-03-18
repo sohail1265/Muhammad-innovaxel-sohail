d923113e9b884006d5f2a5ce81a92abbebb1f50f
sohial1265 <afarmeyo8@gmail.com>
2025-03-18 21:43:39 +0500

    connect github

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
